d095d89536be2f351f1455f02e80a392fed7cb12
Justin H <jhidalgo2@my.wctc.edu>
2022-03-29 22:00:24 -0500

    finished

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 $(function () {
     $("#fetch").on('click',function (e) {
-    let search = $('#artistName').val()
+    let search = $('#artistName').val();
+
     search.replace(' ', '+')
     $.get(
         //endpoint
@@ -8,17 +9,19 @@ $(function () {
         //query params,
         {
             term: search,
-            entity:'musicArtist',
+            entity:'musicArtist, musicTrack',
             limit:10
         },
         //function to call when a result is returned
         function (data) {
-            console.log(data.results[1].artworkUrl100)
-            for (let i = 0; i < 10; i++) {
+            for (let i = 3; i < 10; i++) {
                 $('#result').append(`
-                <div class=" card" style="width: 18rem;">
+                <div class=" card " style="width: 18rem; height: auto">
                   <div class="card-body">
-                    <h5 class="card-title">${data.results[i].artistName}</h5>
+                    <img  class="card-img-top"  src="${data.results[i].artworkUrl60}" alt="">
+                    <h5 class="card-title">Artist: ${data.results[i].artistName}</h5>
+                    <h6 class="card-title">Track Name: ${data.results[i].trackName}</h6>
+                    <h6 class="card-title">Collection Name: ${data.results[i].collectionName}</h6>
                     <p class="card-link">Genre:${data.results[i].primaryGenreName}</p>
                   </div>
                 </div>
